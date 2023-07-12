d24fa547c656e58c6018da32ec2f4f828d84d481
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-12 13:59:56 -0300

    Number

---
+++ 02-Fundamentos/index.js
@@ -0,0 +1,5 @@
+var x = 30;
+var y = 32.3421332;
+console.log(x);
+console.log(typeof x);
+console.log(y.toPrecision(5));
---
+++ 02-Fundamentos/index.ts
@@ -0,0 +1,6 @@
+const x: number = 30;
+const y: number = 32.3421332
+
+console.log(x);
+console.log( typeof x)
+console.log(y.toPrecision(5))
