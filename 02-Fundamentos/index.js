830b6aabfcbfd8853a1749ff8089bf863ce6a8c0
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-12 14:06:34 -0300

    String

--- 02-Fundamentos/index.js
+++ 02-Fundamentos/index.js
@@ -3,3 +3,11 @@ var y = 32.3421332;
 console.log(x);
 console.log(typeof x);
 console.log(y.toPrecision(5));
+//String
+var firstName = "Kaio";
+console.log(firstName);
+console.log(firstName.toUpperCase());
+var fullname;
+var name2 = "Pasqualinotto";
+fullname = "".concat(firstName, " ").concat(name2);
+console.log(fullname);
--- 02-Fundamentos/index.ts
+++ 02-Fundamentos/index.ts
@@ -4,3 +4,16 @@ const y: number = 32.3421332
 console.log(x);
 console.log( typeof x)
 console.log(y.toPrecision(5))
+
+//String
+
+const firstName: string = "Kaio";
+console.log(firstName);
+console.log(firstName.toUpperCase());
+
+let fullname: string;
+let name2: string = "Pasqualinotto";
+
+fullname = `${firstName} ${name2}`
+
+console.log(fullname);
