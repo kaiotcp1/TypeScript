a953da8fb67d9ff9d3a4e802bd27ee08c408f6e8
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-14 17:05:10 -0300

    Array, sintaxe alternativa

--- 03-Tipos/index.js
+++ 03-Tipos/index.js
@@ -6,3 +6,7 @@ console.log(numbers);
 console.log(numbers[3]);
 const nomes = ["Kaio", "Karen"];
 console.log(nomes);
+//Sintaxe diferente
+const nums = [100, 200];
+nums.push(300);
+console.log(nums);
--- 03-Tipos/index.ts
+++ 03-Tipos/index.ts
@@ -5,3 +5,8 @@ console.log(numbers);
 console.log(numbers[3]);
 const nomes: string[] = ["Kaio", "Karen"];
 console.log(nomes);
+
+//Sintaxe diferente
+const nums: Array<number> = [100, 200];
+nums.push(300);
+console.log(nums);
