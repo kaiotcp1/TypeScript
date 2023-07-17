65b7f7451cdd92204c24e46d58b5d6f7a2732a6b
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-17 15:29:42 -0300

    Checagem de valor

--- 04-Narrowing/index.js
+++ 04-Narrowing/index.js
@@ -20,3 +20,27 @@ function sum(a, b) {
 sum('4', '52');
 sum(8, 8);
 sum('5', 3);
+console.log('-----------');
+// Checando se o valor existe
+function operations(arr, operation) {
+    if (operation) { //Narrow
+        if (operation === 'sum') {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+        }
+        else if (operation === 'multiply') {
+            const multiply = arr.reduce((i, total) => i * total);
+            console.log(multiply);
+        }
+        ;
+    }
+    else {
+        console.log('Por favor, defina uma operação !');
+    }
+    ;
+}
+;
+operations([1, 2, 3]);
+operations([1, 2, 3], 'sum');
+operations([2, 4, 8], 'multiply');
+console.log('-----------');
--- 04-Narrowing/index.ts
+++ 04-Narrowing/index.ts
@@ -2,16 +2,37 @@
 Type guard
 É uma validação do tipo utilizando o typeof, comparando o retorno do
 operador com um possível tipo
-*/ 
+*/
 function sum(a: number | string, b: number | string) {
-   if(typeof a === 'string' && typeof b === 'string') {
-    console.log(parseFloat(a) + parseFloat(b))
-   } else if (typeof a === 'number' && typeof b === 'number') {
-    console.log(a + b);
-   } else {
-    console.log('impossível realizar a soma !');
-   };
+    if (typeof a === 'string' && typeof b === 'string') {
+        console.log(parseFloat(a) + parseFloat(b))
+    } else if (typeof a === 'number' && typeof b === 'number') {
+        console.log(a + b);
+    } else {
+        console.log('impossível realizar a soma !');
+    };
 };
 sum('4', '52');
 sum(8, 8);
 sum('5', 3);
+console.log('-----------');
+
+
+// Checando se o valor existe
+function operations(arr: number[], operation?: string | undefined) {
+    if (operation) { //Narrow
+        if (operation === 'sum') {
+            const sum = arr.reduce((i, total) => i + total);
+            console.log(sum);
+        } else if (operation === 'multiply') {
+            const multiply = arr.reduce((i, total) => i * total);
+            console.log(multiply);
+        };
+    } else {
+        console.log('Por favor, defina uma operação !');
+    };
+};
+operations([1, 2, 3]);
+operations([1, 2, 3], 'sum');
+operations([2, 4, 8], 'multiply');
+console.log('-----------');
