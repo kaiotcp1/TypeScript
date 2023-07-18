1384be24e691af5c3369c32585b139c2f47a2f40
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-18 15:14:16 -0300

    Generic function

--- 05-Function/index.js
+++ 05-Function/index.js
@@ -22,3 +22,21 @@ function preGreeting(fn, userName) {
 ;
 preGreeting(greeting, 'Kaio');
 preGreeting(greeting, 'Karen');
+console.log('-------------------');
+//Generic function
+function firstElement(arr) {
+    return arr[0];
+}
+;
+console.log(firstElement([1, 2, 3]));
+console.log(firstElement(['a', 'b', 'c']));
+function mergeObject(obj1, obj2) {
+    return {
+        ...obj1,
+        ...obj2
+    };
+}
+;
+const newObject = mergeObject({ name: 'Kaio' }, { age: 30, job: 'Programmer' });
+console.log(newObject);
+console.log('-------------------');
--- 05-Function/index.ts
+++ 05-Function/index.ts
@@ -3,7 +3,7 @@ Void
 Função sem retorno.
 */
 
-function withoutReturn():void {
+function withoutReturn(): void {
     console.log('Esta função não tem retorno !');
     // return 1;
 }
@@ -12,11 +12,11 @@ console.log('-------------------')
 
 
 //Callback como argumento
-function greeting(name: string):string {
+function greeting(name: string): string {
     return `Olá, ${name}`;
 };
 
-function preGreeting(fn: (name:string) => string, userName:string) {
+function preGreeting(fn: (name: string) => string, userName: string) {
     console.log('Preparando a função !');
     const greet = fn(userName);
     console.log(greet);
@@ -24,3 +24,22 @@ function preGreeting(fn: (name:string) => string, userName:string) {
 
 preGreeting(greeting, 'Kaio');
 preGreeting(greeting, 'Karen');
+console.log('-------------------')
+
+
+//Generic function
+function firstElement<T>(arr: T[]): T {
+    return arr[0];
+};
+console.log(firstElement([1, 2, 3]));
+console.log(firstElement(['a', 'b', 'c']));
+
+function mergeObject<U, T>(obj1: U, obj2: T) {
+    return {
+        ...obj1,
+        ...obj2
+    };
+};
+const newObject = mergeObject({name: 'Kaio'}, {age: 30, job: 'Programmer'});
+console.log(newObject);
+console.log('-------------------');
