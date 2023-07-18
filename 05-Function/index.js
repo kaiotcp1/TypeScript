e424cc3d9eb22dbe03b587b5fd77e314ee668da1
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-18 14:52:31 -0300

    Function void

---
+++ 05-Function/index.js
@@ -0,0 +1,10 @@
+"use strict";
+/*
+Void
+Função sem retorno.
+*/
+function withoutReturn() {
+    console.log('Esta função não tem retorno !');
+    // return 1;
+}
+withoutReturn();
---
+++ 05-Function/index.ts
@@ -0,0 +1,10 @@
+/*
+Void 
+Função sem retorno.
+*/
+
+function withoutReturn():void {
+    console.log('Esta função não tem retorno !');
+    // return 1;
+}
+withoutReturn();
