99e27348af078d5a3309f2c5cb82ad2c307c65c4
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-20 15:15:04 -0300

    Constructor

--- 08-Classes/index.js
+++ 08-Classes/index.js
@@ -8,3 +8,16 @@ kaio.name = 'Kaio';
 kaio.age = 22;
 // kaio.job = 'Programmer';
 console.log(kaio);
+console.log('--------------');
+//Constructor
+class NewUser {
+    constructor(name, age) {
+        this.name = name;
+        this.age = age;
+    }
+    ;
+}
+;
+const Kaio = new NewUser('Kaio', 22);
+console.log(Kaio);
+console.log('--------------');
--- 08-Classes/index.ts
+++ 08-Classes/index.ts
@@ -1,5 +1,4 @@
 //Campos em classe
-
 class User {
     name!: string
     age!: number
@@ -10,3 +9,20 @@ kaio.name = 'Kaio';
 kaio.age = 22;
 // kaio.job = 'Programmer';
 console.log(kaio);
+console.log('--------------');
+
+
+//Constructor
+class NewUser {
+    name;
+    age;
+
+    constructor(name: string, age: number) {
+        this.name = name;
+        this.age = age;
+    };
+};
+
+const Kaio = new NewUser('Kaio', 22);
+console.log(Kaio);
+console.log('--------------');
