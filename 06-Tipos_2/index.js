c9fc88608d6b5ffebaba8779148090e6936df2d5
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-20 14:20:31 -0300

    Reduzindo tipos aceitos em Generics

--- 06-Tipos_2/index.js
+++ 06-Tipos_2/index.js
@@ -7,3 +7,19 @@ function showData(arg) {
 console.log(showData('Kaio'));
 console.log(showData(5));
 console.log('-----------------');
+/*
+Contraint em Generic
+Obrigamos o generic a ter um tipo especifico de propriedade
+dessa forma somos obrigado a satisfazer essa condição.
+no exemplo é um objeto com a propriedade name, as outras
+propriedades não importam ou até mesmo ter apenas o name.
+*/
+function showProductName(obj) {
+    return `O nome do produto é: ${obj.name}`;
+}
+;
+const myObj = { name: 'Porta', cor: 'Branca' };
+// const myObj2 = {cor: 'Branca'};
+console.log(showProductName(myObj));
+// console.log(showProductName(myObj2));
+console.log('-----------------');
--- 06-Tipos_2/index.ts
+++ 06-Tipos_2/index.ts
@@ -1,7 +1,26 @@
 //Generics
-function showData<T>(arg: T): string  {
+function showData<T>(arg: T): string {
     return `O dado é ${arg}`;
 };
 console.log(showData('Kaio'));
 console.log(showData(5));
+console.log('-----------------');
+
+
+/*
+Contraint em Generic
+Obrigamos o generic a ter um tipo especifico de propriedade
+dessa forma somos obrigado a satisfazer essa condição.
+no exemplo é um objeto com a propriedade name, as outras
+propriedades não importam ou até mesmo ter apenas o name.
+*/
+function showProductName<T extends { name: string }>(obj: T) {
+    return `O nome do produto é: ${obj.name}`;
+};
+
+const myObj = {name: 'Porta', cor: 'Branca'};
+// const myObj2 = {cor: 'Branca'};
+
+console.log(showProductName(myObj));
+// console.log(showProductName(myObj2));
 console.log('-----------------');
