99c8b825244986bb55effa80f07618242864dcc3
Kaio Pasqualinotto <kaiotcp1@gmail.com>
2023-07-21 18:19:23 -0300

    Multiplas importações

--- 09-Modules/index.js
+++ 09-Modules/index.js
@@ -6,3 +6,13 @@ Object.defineProperty(exports, "__esModule", { value: true });
 // importação de arquivos
 const greet_js_1 = __importDefault(require("./greet.js"));
 (0, greet_js_1.default)();
+//-------------------------------------------
+//import de variavel;
+const variable_js_1 = require("./variable.js");
+console.log(variable_js_1.x);
+//-------------------------------------------
+//Multiplas importações
+const multiples_js_1 = require("./multiples.js");
+console.log(multiples_js_1.a);
+console.log(multiples_js_1.b);
+(0, multiples_js_1.myFunction)();
--- 09-Modules/index.ts
+++ 09-Modules/index.ts
@@ -1,4 +1,15 @@
 // importação de arquivos
 import importGreet2 from './greet.js';
-
 importGreet2();
+//-------------------------------------------
+
+//import de variavel;
+import { x } from './variable.js';
+console.log(x);
+//-------------------------------------------
+
+//Multiplas importações
+import { a, b, myFunction } from './multiples.js';
+console.log(a);
+console.log(b);
+myFunction();
